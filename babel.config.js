0bccaa4fb84add75778e319a54c8c90b6ab4ade6
Pavel <hawkrammfan@gmail.com>
2022-06-05 18:06:10 +0300

    change babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,16 @@
-module.exports = {
-    presets: [["@babel/preset-env", { targets: { node: "current" } }]],
+module.exports = (api) => {
+  return {
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          useBuiltIns: "entry",
+          corejs: "3.22",
+          targets: api.caller((caller) => caller && caller.target === "node")
+            ? { node: "current" }
+            : { chrome: "58", ie: "11" },
+        },
+      ],
+    ],
   };
-  
+};
