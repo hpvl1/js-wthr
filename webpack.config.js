a681aaf639eacd611c113a5ffb7400cf83fad421
Pavel <hawkrammfan@gmail.com>
2022-06-05 19:40:28 +0300

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
     clean: true,
+    environment: {
+      arrowFunction: false,
+    },
   },
   devtool:
     process.env.NODE_ENV === "development" ? "eval-source-map" : "source-map",
