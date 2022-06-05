9ed83083f37bb4dec45cd6555757d79570b25230
Pavel <hawkrammfan@gmail.com>
2022-06-05 19:17:01 +0300

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     process.env.NODE_ENV === "development" ? "eval-source-map" : "source-map",
   plugins: [
     new HtmlWebpackPlugin({
-      template: "index.html",
+      template: path.resolve(__dirname, "./index.html"),
     }),
   ],
   module: {
