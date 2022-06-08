4ced3a794dbc402de729eef3bb39b65930578904
Pavel <hawkrammfan@gmail.com>
2022-06-08 21:52:21 +0300

    fix eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,26 @@
 module.exports = {
-	env: {
-	  browser: true,
-	  es2021: true,
-	  "jest/globals": true,
-	  node: true,
-	},
-	extends: ["eslint:recommended", "prettier"],
-	parserOptions: {
-	  ecmaVersion: "latest",
-	  sourceType: "module",
-	},
-	plugins: ["jest"],
-	rules: {
-	  "import/prefer-default-export": "off",
-	  "no-unused-vars": "warn",
-	  "no-useless-escape": "warn",
-	  "max-len": [
-		"error",
-		{
-		  code: 120,
-		  ignoreComments: true,
-		},
-	  ],
-	},
-  };
-  
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["jest"],
+  rules: {
+    "import/prefer-default-export": "off",
+    "no-unused-vars": "warn",
+    "no-useless-escape": "warn",
+    "max-len": [
+      "error",
+      {
+        code: 180,
+        ignoreComments: true,
+      },
+    ],
+  },
+};
