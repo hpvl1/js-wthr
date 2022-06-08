f54bd6adefbd0bd3e86c18ecf59420648e3fd065
Pavel <hawkrammfan@gmail.com>
2022-06-08 22:13:20 +0300

    add getCityInput.js

---
+++ src/getCityInput.js
@@ -0,0 +1,16 @@
+export async function getCityInput(cityName) {
+  const MY_KEY = "56f47affbfbb9ca1677d4f24e14ea341";
+
+  try {
+    const response = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&lang=ru&units=metric&appid=${MY_KEY}`
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
