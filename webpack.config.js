76420de54b1b5363bcabff2f3b163e4f1b7f8391
Pavel <hawkrammfan@gmail.com>
2022-06-15 13:47:58 +0300

    remove postCss & postCSS-loader

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.scss$/,
