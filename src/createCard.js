5076ad06b68333d5a3bd12041758224a052b216a
Pavel <hawkrammfan@gmail.com>
2022-06-17 14:25:01 +0300

    change createCard.js

--- src/createCard.js
+++ src/createCard.js
@@ -2,15 +2,23 @@
 export function createCard(el, weatherData) {
   el.innerHTML = `
         <div class="weather-wrap">
+          <div class="list-city">
+            <ul></ul>
+          </div>
+          <div class="city-info">
             <p class="city"><span><b>Город: </b></span>${weatherData.name}</p>
-            <p class="temp"><span><b>Температура: </b></span>${
-              weatherData.main.temp - 273
+            <p class="temp"><span><b>Температура: </b></span>${weatherData.main.temp.toFixed(
+              1
+            )} &#8451;</p>
+            <p class="weather"><span><b>Погода: </b></span>${
+              weatherData.weather[0].description
             }</p>
             <div class="icon">
                 <img src = "https://openweathermap.org/img/wn/${
                   weatherData.weather[0].icon
                 }@2x.png">
-            </div>
+          </div>
+        </div>
     `;
   createMap(el, weatherData);
 }
@@ -31,5 +39,12 @@ function createMap(el, weatherData) {
       // Уровень масштабирования. Допустимые значения: от 0 (весь мир) до 19.
       zoom: 12,
     });
+
+    var myPlacemark = new ymaps.Placemark([
+      weatherData.coord.lat,
+      weatherData.coord.lon,
+    ]);
+
+    myMap.geoObjects.add(myPlacemark);
   }
 }
